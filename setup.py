e5f2f34a0406b36eb938ec6cef30c28a05c806f8
Miguel Cabrera <miguel.cabrera@trustyou.net>
2014-11-12 12:25:40 +0100

    Removing unused include folders

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.platform == 'darwin':
     ext_modules = [Extension(
                    name='bh_sne',
                    sources=['tsne/bh_sne_src/quadtree.cpp', 'tsne/bh_sne_src/tsne.cpp', 'tsne/bh_sne.pyx'],
-                   include_dirs=[numpy.get_include(), 'tsne/bh_sne_src/', '/usr/local/opt/openblas/include'],                  
+                   include_dirs=[numpy.get_include(), 'tsne/bh_sne_src/'],                  
                    extra_compile_args=['-stdlib=libc++', '-mmacosx-version-min=10.9'],
                    extra_link_args=['-stdlib=libc++','-mmacosx-version-min=10.9', '-Wl,-framework', '-Wl,Accelerate', '-lcblas'],
                    language='c++')]
